2d0be07676aca44ceeae6c66082ed5e293a3ebad
jsgabriel1 <93747938+jsgabriel1@users.noreply.github.com>
2025-10-02 15:44:12 -0300

    Update d-cohen.R

--- master_project/scripts/d-cohen.R
+++ master_project/scripts/d-cohen.R
@@ -1,31 +1,35 @@
-#Instalando o pacote 
+#Instalando os pacotes 
+install.packages("tidyverse")
 install.packages("effectsize")
 library(readxl)
 library(effectsize)
+library(tidyverse)
 
 #Importando as planilhas
-
 br_n2<-read_excel("br_final.xlsx")
-ti<-read.csv("tabela_final_ind1.csv", dec=",")
-quilo<-read.csv("tabela_final_quilo1.csv", dec=",")
+ti<-read_excel("tabela_final_ind.xlsx")
+quilo<--read_excel("tabela_final_quilo.xlsx")
 
 glimpse(br_n2)
 
 d1<-ti%>%
-  filter(NM_REGIAO=="Nordeste")
+  filter(NM_REGIAO=="Norte")
 d2<-br_n2%>%
-  filter(regiao=="NORDESTE")
+  filter(regiao=="NORTE")
 
-d3<-d1$seca_segali
-d4<-d2$seca_segali
+d3<-d1$saude_leishmaniose_tegumentar
+d4<-d2$saude_leishmaniose_tegumentar
 
 #d-COHEN geral 
 resultado <- cohens_d(d3,d4)
-media_ti<-mean(d3)
-media_regiao<-mean(d4)
+media_ti<-mean(d3, na.rm=TRUE)
+media_regiao<-mean(d4, na.rm=TRUE)
 
-# Exibir o resultado
-cat("Média TI/Quilombo:", media_ti,"|", "Média região:", media_regiao)
-print(resultado)
+# Exibir resultados com mais casas decimais
+cat("Média TI/Quilombo:", sprintf("%.10f", media_ti), "\n")
+cat("Média Região:", sprintf("%.10f", media_regiao), "\n\n")
+
+# Mostrar resultado com mais casas decimais no IC
+print(format(resultado, digits = 15))
 
 
